0cfd897560edddf863513ec142cfceca73e19334
gurbetarslan <gurbetarslan94@gmail.com>
2020-05-09 13:26:24 +0300

    ButtonCustom and DrawableHamburgerMenu

--- app/src/main/java/com/example/otopark/BaseActivity.kt
+++ app/src/main/java/com/example/otopark/BaseActivity.kt
@@ -1,4 +1,5 @@
 package com.example.otopark
+
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -6,92 +7,80 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
 
-class BaseActivity() : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
-    lateinit var profileFragment: ProfileFragment
-    lateinit var anaEkranFragment: AnaEkranFragment
-    lateinit var plakalarFragment: PlakalarFragment
-    lateinit var rezervasyonlarFragment: RezervasyonlarFragment
-    lateinit var cuzdanimFragment: CuzdanimFragment
-    lateinit var odemeAraclarimFragment: OdemeAraclarimFragment
+open class BaseActivity() : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_solmenudrawable)
+        setContentView(R.layout.activity_base)
 
+        toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
         setSupportActionBar(toolbar)
-        val actionBar=supportActionBar
-        actionBar?.title="E-Park"
+        val actionBar = supportActionBar
+        actionBar?.title = "E-Park"
 
-        val drawerToggle:ActionBarDrawerToggle=object : ActionBarDrawerToggle(
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
             this,
             drawerLayout,
             toolbar,
             (R.string.open),
             (R.string.close)
-        ){
+        ) {
 
         }
-        drawerToggle.isDrawerIndicatorEnabled =true
+        drawerToggle.isDrawerIndicatorEnabled = true
         drawerLayout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
-        navView.setNavigationItemSelectedListener (this)
+        navView.setNavigationItemSelectedListener(this)
 
-        anaEkranFragment= AnaEkranFragment()
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.frame_layout,anaEkranFragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .add(R.id.frame_layout, AnaEkranFragment(), "anaEkranFragment")
             .commit()
 
     }
+
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-        when (menuItem.itemId){
+        when (menuItem.itemId) {
             R.id.nav_hesabim -> {
-               profileFragment=ProfileFragment()
                 supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.frame_layout,profileFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .replace(R.id.frame_layout, ProfileFragment(), "profileFragment")
                     .commit()
             }
 
             R.id.nav_plakalarim -> {
-                plakalarFragment= PlakalarFragment()
                 supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.frame_layout,plakalarFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .replace(R.id.frame_layout, PlakalarFragment(), " plakalarFragment")
                     .commit()
             }
-            R.id.nav_rezervasyonlarim -> {
-                rezervasyonlarFragment= RezervasyonlarFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,rezervasyonlarFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
-            R.id.nav_cuzdanim -> {
-                cuzdanimFragment= CuzdanimFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,cuzdanimFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
-            R.id.nav_odeme_araclarim -> {
-                odemeAraclarimFragment= OdemeAraclarimFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,odemeAraclarimFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-            }
+//            R.id.nav_rezervasyonlarim -> {
+//                rezervasyonlarFragment = RezervasyonlarFragment()
+//                supportFragmentManager
+//                    .beginTransaction()
+//                    .replace(R.id.frame_layout, rezervasyonlarFragment)
+//                    .commit()
+//            }
+//            R.id.nav_cuzdanim -> {
+//                cuzdanimFragment = CuzdanimFragment()
+//                supportFragmentManager
+//                    .beginTransaction()
+//                    .replace(R.id.frame_layout, cuzdanimFragment)
+//                    .commit()
+//            }
+//            R.id.nav_odeme_araclarim -> {
+//                odemeAraclarimFragment = OdemeAraclarimFragment()
+//                supportFragmentManager
+//                    .beginTransaction()
+//                    .replace(R.id.frame_layout, odemeAraclarimFragment)
+//                    .commit()
+//            }
         }
 
         drawerLayout.closeDrawer(GravityCompat.START)
@@ -99,10 +88,9 @@ class BaseActivity() : AppCompatActivity(), NavigationView.OnNavigationItemSelec
 
     }
     override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
-        }
-        else {
+        } else {
             super.onBackPressed()
         }
     }
---
+++ app/src/main/java/com/example/otopark/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.example.otopark
+class MainActivity : BaseActivity() {
+
+}
