cb665bba7d5beb08295441c51847c39f54e24ed5
cigdemdurman <Drmn1991>
2020-05-15 21:51:55 +0300

    login  sayfası eklendi

---
+++ app/src/main/java/com/example/otopark/BuildConfig.kt
@@ -0,0 +1,12 @@
+package com.example.otopark
+
+import java.lang.Boolean
+
+object BuildConfig {
+    val DEBUG = Boolean.parseBoolean("true")
+    const val APPLICATION_ID = "com.example.loginapp"
+    const val BUILD_TYPE = "debug"
+    const val FLAVOR = ""
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+}
