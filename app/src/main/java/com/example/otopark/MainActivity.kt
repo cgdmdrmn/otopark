2071bc0b0285e3cd2bb97b4541e28fb7f9c1a028
cigdemdurman <Drmn1991>
2020-05-15 23:15:48 +0300

    login ve kayıt ol sayfası eklendi

--- app/src/main/java/com/example/otopark/MainActivity.kt
+++ app/src/main/java/com/example/otopark/MainActivity.kt
@@ -1,4 +1,42 @@
 package com.example.otopark
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.loginapp.LoginFragment
+import java.util.*
+
+@Suppress("DEPRECATION")
 class MainActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val viewPager: ViewPager = findViewById(R.id.viewPager)
+        val pagerAdapter =
+            AuthenticationPagerAdapter(supportFragmentManager)
+        pagerAdapter.addFragmet(LoginFragment())
+        pagerAdapter.addFragmet(RegisterFragment())
+        viewPager.adapter = pagerAdapter
+    }
+
+    internal class AuthenticationPagerAdapter(fm: FragmentManager?) :
+        FragmentPagerAdapter(fm!!) {
+        private val fragmentList = ArrayList<Fragment>()
+        override fun getItem(i: Int): Fragment {
+            return fragmentList[i]
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        fun addFragmet(fragment: Fragment) {
+            fragmentList.add(fragment)
+        }
+    }
 
 }
