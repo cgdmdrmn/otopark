297954273f264fbdf9a1bd092e520eb5481f3a18
cigdemdurman <Drmn1991>
2020-05-16 00:17:28 +0300

    login ve kayıt ol sayfası eklendi

--- app/src/main/java/com/example/otopark/MainActivity.kt
+++ app/src/main/java/com/example/otopark/MainActivity.kt
@@ -1,42 +1,58 @@
 package com.example.otopark
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.loginapp.LoginFragment
+import kotlinx.android.synthetic.main.activity_base.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.viewPager
 import java.util.*
+import kotlin.collections.ArrayList
 
 @Suppress("DEPRECATION")
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val viewPager: ViewPager = findViewById(R.id.viewPager)
-        val pagerAdapter =
-            AuthenticationPagerAdapter(supportFragmentManager)
-        pagerAdapter.addFragmet(LoginFragment())
-        pagerAdapter.addFragmet(RegisterFragment())
-        viewPager.adapter = pagerAdapter
+
+        val adapter = MyViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(LoginFragment() , title = "Giriş  Ekranı")
+        adapter.addFragment(RegisterFragment() , title = "Kayıt  Ekranı")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
     }
 
-    internal class AuthenticationPagerAdapter(fm: FragmentManager?) :
-        FragmentPagerAdapter(fm!!) {
-        private val fragmentList = ArrayList<Fragment>()
-        override fun getItem(i: Int): Fragment {
-            return fragmentList[i]
+    class MyViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+
+        private val fragmentList : MutableList<Fragment> = ArrayList ()
+        private val titleList : MutableList<String> = ArrayList ()
+        override fun getItem(position: Int): Fragment {
+                return fragmentList [position]
         }
 
         override fun getCount(): Int {
             return fragmentList.size
+
         }
 
-        fun addFragmet(fragment: Fragment) {
+        fun addFragment(fragment: Fragment,title:String){
             fragmentList.add(fragment)
+            titleList.add(title)
+        }
+
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return titleList[position]
         }
     }
+
+
 
 }
